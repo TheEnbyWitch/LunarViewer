ed394a45326709d017c115744f1faa18485f4b36
Luna Ryuko Zaremba <contact@lunaryuko.space>
2021-07-12 11:36:54 +0200

    (hopefully) fix __VA_ARGS__ for linux

--- src/Common.h
+++ src/Common.h
@@ -26,7 +26,7 @@ char* va(const char* fmt, ...);
 void Com_Printf(char* fmt, ...);
 
 #define Com_Error(level, fmt, ...) \
-	Com_ErrorEx(level, va("%s:%d -> %s()", __FILE__, __LINE__, __FUNCTION__), va(fmt, __VA_ARGS__));
+	Com_ErrorEx(level, va("%s:%d -> %s()", __FILE__, __LINE__, __FUNCTION__), va(fmt,## __VA_ARGS__));
 
 void Com_ErrorEx(int level, char* source, char* msg);
 
